e64eceb806b59c7dcc8c45b9f6b41e7eb1271a3a
Kim Ga Young <bunny0098@naver.com>
2020-06-02 22:28:36 +0900

    all tasks challenges + sign_up

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -11,12 +11,12 @@ class BooksInstanceInline(admin.TabularInline):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author','display_genre')
+    list_display = ('title', 'author','language','display_genre')
     inlines = [BooksInstanceInline]
     
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
-    #list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, {'fields': ('book', 'imprint', 'id')}),
@@ -26,3 +26,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Language)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -24,7 +24,7 @@ class Book(models.Model):
     
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
@@ -96,5 +96,9 @@ class Author(models.Model):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
 
+class Language(models.Model):
+    name = models.CharField(max_length=200,help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
 
+    def __str__(self):
+        return self.name
 
